1b48bb1401fbcf21df1faf35b1d58922a322566d
JumaMac <mjustinmacaraig@gmail.com>
2022-07-22 14:42:40 -0400

    added inventory list headers, making the inventory cards afterwards

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import WarehouseDetails from "./Components/WarehouseDetails/WarehouseDetails"
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
 
-        <Route path="/" exact component={Warehouses} />
-        <Route path="/inventory" component={Inventory} />
+        {/* <Route path="/warehouse" exact component={Warehouses} />
+        <Route path="/inventory" component={Inventory} /> */}
+        <Route path="/warehouse/:id" render={(props) => {
+          return <WarehouseDetails {...props} />;
+        }} />
 
       </Switch>
     </BrowserRouter>  
--- src/Components/WarehouseDetails/WarehouseDetails.jsx
+++ src/Components/WarehouseDetails/WarehouseDetails.jsx
@@ -4,11 +4,12 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import backArrow from "../../assets/Icons/arrow_back-24px.svg";
 import editIcon from "../../assets/Icons/edit-24px.svg";
+import sortIcon from "../../assets/Icons/sort-24px.svg";
 const api = "http://localhost:8080"
 
 function WarehouseDetails(props) {
-    const [warehouse, setWarehouse] = useState([]);
-    const [inventory, setInventory] = useState([]);
+    const [warehouse, setWarehouse] = useState(null);
+    // const [inventory, setInventory] = useState([]);
     const warehouseID = props.match.params.id;
 
     useEffect((res) => {
@@ -16,16 +17,19 @@ function WarehouseDetails(props) {
         .then((res) => {
             if(res.status === 200){
                 setWarehouse(res.data);
+                console.log(res.data);
             }
         });
 
-        axios.get(`${api}/warehouse/${warehouseID}/inventory`)
-        .then((res) => {
-            if(res.status === 200){
-                setInventory(res.data);
-            }
-        });
-    }, []);
+        // axios.get(`${api}/warehouse/${warehouseID}/inventory`)
+        // .then((res) => {
+        //     if(res.status === 200){
+        //         setInventory(res.data);
+        //     }
+        // });
+    },[warehouseID]);
+
+    if (!warehouse) return null;
 
     return(
         <section className="warehouse-details">
@@ -43,10 +47,54 @@ function WarehouseDetails(props) {
                     </Link>
                 </div>
             </article>
-            <article>
-                
+            <article className="details">
+                <div>
+                    <h6 className="details__subheader">WAREHOUSE ADDRESS:</h6>
+                    <p className="details__value">{warehouse.address}, {warehouse.city}, {warehouse.country}</p>
+                </div>
+                <div className="details__contact-container">
+                    <div className="details__name-container">
+                        <h6 className="details__subheader">CONTACT NAME:</h6>
+                        <p className="details__value">{warehouse.contact.name}</p>
+                        <p className="details__value">{warehouse.contact.position}</p>
+                    </div>
+                    <div className="details__info-container">
+                        <h6 className="details__subheader">CONTACT INFORMATION:</h6>
+                        <p className="details__value">{warehouse.contact.phone}</p>
+                        <p className="details__value">{warehouse.contact.email}</p>
+                    </div>
+                </div>
             </article>
+            <article className="inventory-list__header">
+
+                <div className="inventory-list__header--left">
+                    <div className="inventory-list__header--sub">
+                        <p className="inventory_list__header--text">INVENTORY ITEM</p>
+                        <img className="inventory-list__header--icon" src={sortIcon} alt="sort icon" />
+                    </div>
+                    <div className="inventory-list__header--sub">
+                        <p className="inventory_list__header--text">CATEGORY</p>
+                        <img className="inventory-list__header--icon" src={sortIcon} alt="sort icon" />
+                    </div>
+                </div>
+
+                <div className="inventory-list__header--right">
+                    <div className="inventory-list__header--sub">
+                        <p className="inventory_list__header--text">STATUS</p>
+                        <img className="inventory-list__header--icon" src={sortIcon} alt="sort icon" />
+                    </div>
+                    <div className="inventory-list__header--sub">
+                        <p className="inventory_list__header--text">QUANTITY</p>
+                        <img className="inventory-list__header--icon" src={sortIcon} alt="sort icon" />
+                    </div>
+                    <div className="inventory-list__header--actions">
+                        <p className="inventory_list__header--text">ACTIONS</p>
+                    </div>
+                </div>
+            </article>
+
         </section>
     )
-
 }
+
+export default WarehouseDetails;
