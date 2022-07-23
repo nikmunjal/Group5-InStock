7e31aca51b940008284f389f65ed5d271e8c7e15
andrewssito <aksito1102@gmail.com>
2022-07-23 02:36:40 -0700

    mobile styling done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
-import headerLogo from '../assets/Logo/InStock-Logo_1x.png';
-
+import headerLogo from '../assets/Logo/InStock-Logo.svg';
 
 
 function Header() {
@@ -9,7 +8,18 @@ function Header() {
             
             <img className="header__logo" src={headerLogo} alt="instock logo"/>
 
-            
+            <section className="header__links">
+                <section className="header__links-left">
+                    <button className="warehouses__link">
+                        Warehouses
+                    </button>
+                </section>
+                <section className="header__links-right">
+                    <button className="inventory__link">
+                        Inventory
+                    </button>
+                </section>
+            </section>
 
         </div>
     )
