39affb45cafcd460ac59de30f202e6092271b6ba
andrewssito <aksito1102@gmail.com>
2022-07-23 08:33:35 -0700

    set up component and hook

---
+++ src/Components/InventoryDelete/InventoryDelete.js
@@ -0,0 +1,34 @@
+import closeIcon from '../../assets/Icons/close-24px.svg';
+
+
+
+function InventoryDelete(props) {
+
+
+    return (
+        <div className="inventory__delete">
+
+            <img className="close__icon" src={closeIcon} alt="close icon"/>
+
+            <h1 className="inventory__delete-header">
+                Delete {props.itemName} inventory item?
+            </h1>
+
+            <p className="inventory__delete-desc">
+                Please confirm that you'd like to delete {props.itemName} from the inventory list. You won't be able to undo this action.
+            </p>
+
+            <section className="inventory__delete-options">
+                <button className="inventory__cancel-button">
+                    Cancel
+                </button>
+                <button className="inventory__delete-button">
+                    Delete
+                </button>
+
+            </section>
+        </div>
+    )
+};
+
+export default InventoryDelete;
--- src/page/InventoryPage/InventoryPage.js
+++ src/page/InventoryPage/InventoryPage.js
@@ -1,7 +1,18 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import InventoryDelete from "../../components/InventoryDelete/InventoryDelete";
+
+import axios from "axios";
+import React, { useState, useEffect } from 'react';
 
 function InventoryPage() {
+
+    // to set item based on id
+    const [item, setItem] = useState('');
+
+    useEffect(() => {
+        
+    })
 
     return (
         <div>
