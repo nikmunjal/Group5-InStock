66a18f780f741f7d11d920124f0e47e8edf1b8f5
andrewssito <aksito1102@gmail.com>
2022-07-23 05:47:12 -0700

    footer styling done

--- src/page/InventoryPage/InventoryPage.js
+++ src/page/InventoryPage/InventoryPage.js
@@ -1,11 +1,13 @@
 import Header from "../../components/Header";
-
+import Footer from "../../components/Footer";
 
 function InventoryPage() {
 
     return (
         <div>
             <Header />
+
+            <Footer />
         </div>
     )
 
