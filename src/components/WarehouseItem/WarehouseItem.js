b037d73bd3291fcb76af7bc0bc3e6d8a2ddce8ef
JumaMac <mjustinmacaraig@gmail.com>
2022-07-24 16:37:22 -0400

    finished styling for addinv page and warehouse page

--- src/components/WarehouseItem/WarehouseItem.js
+++ src/components/WarehouseItem/WarehouseItem.js
@@ -1,6 +1,7 @@
 import './WarehouseItem.scss';
 import delete__icon from "../../assets/Icons/delete_outline-24px.svg";
 import edit__icon from "../../assets/Icons/edit-24px.svg";
+import { Link } from 'react-router-dom';
 
 
 
@@ -9,7 +10,9 @@ function WarehouseItem(props)
   return (
       <>
           <ul className='warehosue__details-list'>
-            <li className='warehouse__detail'>{props.name}</li>
+            <Link to={`/${props.id}`}>
+              <li className='warehouse__detail'>{props.name}</li>
+            </Link>
             <li className='warehouse__detail'>{props.address}</li>
             <li className='warehouse__detail'>{props.contactName}</li>
             <li className='warehouse__detail'>{props.contactPhone} {props.contactEmail}</li>
