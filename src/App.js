709b732d79ba48979c139b488650095a0cc31150
khalidzein83 <khalid.zein1@gmail.com>
2022-07-24 13:42:22 -0400

    tested the post but did not get the phone and email validation to work

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import InventoryPage from './page/InventoryPage/InventoryPage';
-import WarehourePage from './page/WarehousePage/WarehousePage';
-import InventoryDetailsPage from './page/InventoryDetailsPage/InventoryDetailsPage';
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import InventoryPage from "./page/InventoryPage/InventoryPage";
+import WarehourePage from "./page/WarehousePage/WarehousePage";
+import InventoryDetailsPage from "./page/InventoryDetailsPage/InventoryDetailsPage";
+import AddNewWareHouse from "./page/AddNewWareHouse/AddNewWareHouse";
+import EditWarehouse from "./page/EditWarehouse/EditWarehouse";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-
-        <Route path="/" exact component={WarehourePage} />
-        <Route path="/inventory" exact component={InventoryPage} />
-        <Route path="/inventory/:id" component={InventoryDetailsPage} />
-      </Switch>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={WarehourePage} />
+                <Route path="/inventory" exact component={InventoryPage} />
+                <Route path="/inventory/:id" component={InventoryDetailsPage} />
+                <Route path="/addwarehouse" component={AddNewWareHouse} />
+                <Route path="/editwarehouse/:id" component={EditWarehouse} />
+            </Switch>
+        </BrowserRouter>
+    );
 }
-
 
 export default App;
